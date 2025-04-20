3cb0c1f81e864faffd4210c7b7ffb1aa324c2a7b
Marwan Mostafa <marwanm.dev@gmail.com>
2025-04-20 09:41:39 +0200

    feat(chatbot): Add pattern-matching stubs for chatbot input detection

---
+++ chatbot_service/src/main/scala/backend/utils/PatternUtils.scala
@@ -0,0 +1,12 @@
+package backend.utils
+
+object PatternUtils {
+  // TODO: Check if the input contains any common greetings ("hi", "hello", "hey", "greetings")
+  def isGreeting(input: String): Boolean = ???
+
+  // TODO: Check if the input contains the word "quiz"
+  def isQuizRequest(input: String): Boolean = ???
+
+  // TODO: Check if the input contains the word "analytics"
+  def isAnalyticsRequest(input: String): Boolean = ???
+}
