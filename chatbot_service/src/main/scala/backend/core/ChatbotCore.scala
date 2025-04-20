c2d03be94e659a16c5913abe0b1b15552bde9bf0
Marwan Mostafa <marwanm.dev@gmail.com>
2025-04-20 09:47:52 +0200

    feat(chatbot): Implement function to generate contextual responses based on query keywords

---
+++ chatbot_service/src/main/scala/backend/core/ChatbotCore.scala
@@ -0,0 +1,6 @@
+package backend.core
+
+object ChatbotCore {
+  // TODO: Generate a contextual response string based on keyword matching in the query
+  def generateResponse(query: String): String = ???
+}
