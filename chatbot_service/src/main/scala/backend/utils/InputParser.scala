2c8c4179615ff07fc66861fcb73b600f5ac97c4b
Marwan Mostafa <marwanm.dev@gmail.com>
2025-04-20 09:40:44 +0200

    feat(chatbot): scaffold InputParser with cleaning and parsing stubs

---
+++ chatbot_service/src/main/scala/backend/utils/InputParser.scala
@@ -0,0 +1,8 @@
+package backend.utils
+
+object InputParser {
+  // TODO: Tokenizes input string into a list of words
+  def parseInput(input: String): List[String] = ???
+  // TODO: Removes punctuation and converts input to lowercase
+  def cleanInput(input: String): String = ???
+}
