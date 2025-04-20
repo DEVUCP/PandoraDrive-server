d37691498b220c90058e639bf7c03fe55ddebe54
Marwan Mostafa <marwanm.dev@gmail.com>
2025-04-20 09:48:38 +0200

    feat(chatbot): Implement function to start quiz session, ask questions, and log interactions

---
+++ chatbot_service/src/main/scala/backend/core/QuizEngine.scala
@@ -0,0 +1,9 @@
+package backend.core
+
+// import backend.utils.QuizUtils
+// import backend.utils.AnalyticsUtils
+
+object QuizEngine {
+  // TODO: Start a quiz session for the given topic, ask questions, evaluate answers, log interactions, and return a summary
+  def startQuiz(topic: String): String = ???
+}
