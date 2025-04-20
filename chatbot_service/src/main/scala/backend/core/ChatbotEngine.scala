0622a6ad8f07eb8fafa1e0a5e34308f33b040bfe
Marwan Mostafa <marwanm.dev@gmail.com>
2025-04-20 09:47:21 +0200

    feat(chatbot): Add ChatbotEngine stubs for greeting and input handling

---
+++ chatbot_service/src/main/scala/backend/core/ChatbotEngine.scala
@@ -0,0 +1,11 @@
+package backend.core
+
+// import backend.utils.{InputParser, PatternUtils, QuizUtils, AnalyticsUtils, Preferences}
+
+object ChatbotEngine {
+  // TODO: Return a welcome greeting for the user
+  def greetUser(): String = ???
+
+  // TODO: Process cleaned user input using pattern matching to respond appropriately
+  def handleUserInput(input: String): String = ???
+}
