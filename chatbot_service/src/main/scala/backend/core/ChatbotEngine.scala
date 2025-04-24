9ff90acd8edf0f1ad7bfbbdfe36df1f1e3d69b98
Marwan Mostafa <marwanm.dev@gmail.com>
2025-04-24 21:35:28 +0200

    refactor(chatbot): Refactor imports

--- chatbot_service/src/main/scala/backend/core/ChatbotEngine.scala
+++ chatbot_service/src/main/scala/backend/core/ChatbotEngine.scala
@@ -1,10 +1,6 @@
 package backend.core
 
-import backend.utils.AnalyticsUtils
-import backend.utils.InputParser
-import backend.utils.PatternUtils
-import backend.utils.QuizUtils
-import backend.utils.UserPreferences
+import backend.utils.{InputParser, PatternUtils, QuizUtils, AnalyticsUtils, Preferences}
 
 object ChatbotEngine {
   def greetUser(): String = 
