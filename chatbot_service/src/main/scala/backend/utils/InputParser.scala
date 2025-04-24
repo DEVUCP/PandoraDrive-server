da4af9dcfdc3d437652b5fe09f6c73e4427b8958
Abdelrahman103 <abdelrahmanamryoussefmahmoud@gmail.com>
2025-04-24 18:24:08 +0200

    feat(chatbot): Implement cleanInput

--- chatbot_service/src/main/scala/backend/utils/InputParser.scala
+++ chatbot_service/src/main/scala/backend/utils/InputParser.scala
@@ -1,8 +1,8 @@
 package backend.utils
-
 object InputParser {
+  def cleanInput(input: String): String = {
+    input.replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase.trim
+  }
   // TODO: Tokenizes input string into a list of words
   def parseInput(input: String): List[String] = ???
-  // TODO: Removes punctuation and converts input to lowercase
-  def cleanInput(input: String): String = ???
 }
