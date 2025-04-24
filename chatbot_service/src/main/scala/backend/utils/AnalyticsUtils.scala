d33a4a572c5a5333c44bc5ac0e89ffdb6d7dc8df
Abdelrahman103 <abdelrahmanamryoussefmahmoud@gmail.com>
2025-04-24 17:41:37 +0200

    feat(chatbot): Implement logInteraction

--- chatbot_service/src/main/scala/backend/utils/AnalyticsUtils.scala
+++ chatbot_service/src/main/scala/backend/utils/AnalyticsUtils.scala
@@ -1,12 +1,11 @@
 package backend.utils
-
 object AnalyticsUtils {
   private var log: List[(Int, String, String)] = List()
   private var counter: Int = 0
-
-  // TODO: Log a user input and corresponding chatbot response, incrementing the interaction counter
-  def logInteraction(userInput: String, chatbotResponse: String): Unit = ???
-
+  def logInteraction(userInput: String, chatbotResponse: String): Unit = {
+    counter += 1
+    log = log :+ (counter, userInput, chatbotResponse)
+  }
   // TODO: Return the full list of logged interactions with sequence numbers
   def getInteractionLog(): List[(Int, String, String)] = ???
 }
