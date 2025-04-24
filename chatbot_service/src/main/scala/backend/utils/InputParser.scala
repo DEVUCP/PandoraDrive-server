3248a9d695b4b9ab007dcda0ca36cc332a349b1d
Abdelrahman103 <abdelrahmanamryoussefmahmoud@gmail.com>
2025-04-24 18:25:14 +0200

    feat(chatbot): Implement parseInput

--- chatbot_service/src/main/scala/backend/utils/InputParser.scala
+++ chatbot_service/src/main/scala/backend/utils/InputParser.scala
@@ -3,6 +3,7 @@ object InputParser {
   def cleanInput(input: String): String = {
     input.replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase.trim
   }
-  // TODO: Tokenizes input string into a list of words
-  def parseInput(input: String): List[String] = ???
+  def parseInput(input: String): List[String] = {
+    cleanInput(input).split("\\s+").toList
+  }
 }
