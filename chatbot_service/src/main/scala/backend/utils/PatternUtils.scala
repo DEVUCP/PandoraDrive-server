bc981d7d6ef3a63254c743001e9f568ef4a0c5d1
Marwan Mostafa <marwanm.dev@gmail.com>
2025-04-24 21:22:30 +0200

    refactor(chatbot): Refactor isGreeting

--- chatbot_service/src/main/scala/backend/utils/PatternUtils.scala
+++ chatbot_service/src/main/scala/backend/utils/PatternUtils.scala
@@ -1,6 +1,7 @@
 package backend.utils
 object PatternUtils {
   def isGreeting(input: String): Boolean = {
+    val greetings = List("hello", "hi", "hey", "greetings", "greeting")
     val words = input.toLowerCase.split("\\s+").toSet
     greetings.exists(words.contains)
   }
