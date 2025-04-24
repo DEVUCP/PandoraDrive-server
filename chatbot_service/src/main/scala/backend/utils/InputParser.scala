857b814d617707e1d1d2eb690f1ec571f1ac799b
Marwan Mostafa <marwanm.dev@gmail.com>
2025-04-24 21:25:41 +0200

    refactor(chatbot): Change parseInput to be more inclusive

--- chatbot_service/src/main/scala/backend/utils/InputParser.scala
+++ chatbot_service/src/main/scala/backend/utils/InputParser.scala
@@ -1,8 +1,7 @@
 package backend.utils
 object InputParser {
-  def cleanInput(input: String): String = {
-    input.replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase.trim
-  }
+  def cleanInput(input: String): String =
+    input.replaceAll("""[\p{Punct}]""", "").toLowerCase.trim
   def parseInput(input: String): List[String] = {
     cleanInput(input).split("\\s+").toList
   }
