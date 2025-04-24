e4a2531f0b46412c62ce2fc55af1c8c1ce927987
Marwan Mostafa <marwanm.dev@gmail.com>
2025-04-24 21:27:01 +0200

    style(chatbot): Add new lines

--- chatbot_service/src/main/scala/backend/utils/AnalyticsUtils.scala
+++ chatbot_service/src/main/scala/backend/utils/AnalyticsUtils.scala
@@ -1,10 +1,13 @@
 package backend.utils
+
 object AnalyticsUtils {
   private var log: List[(Int, String, String)] = List()
   private var counter: Int = 0
+
   def logInteraction(userInput: String, chatbotResponse: String): Unit = {
     counter += 1
     log = log :+ (counter, userInput, chatbotResponse)
   }
+
   def getInteractionLog(): List[(Int, String, String)] = log
 }
