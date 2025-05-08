1833586f8aba5f84e6025e861153b5be5c5e0393
DEVUCP <OMHN05@protonmail.com>
2025-05-08 18:20:21 +0300

    fix(gateway): Update `FileRoutes` endpoints to match new and updated file_services endpoints

--- api_gateway_service/src/main/scala/backend/routes/FileRoutes.scala
+++ api_gateway_service/src/main/scala/backend/routes/FileRoutes.scala
@@ -15,54 +15,16 @@ import io.circe.Json
 import io.circe.generic.auto._
 import org.http4s.implicits._
 import utils.config
+import utils.routing.routeRequestJson
+
 
 object FileRoutes {
 
   val file_service_port = config.FILE_SERVICE_PORT
 
-   def routeRequestImpl[T](req: Request[IO], uriString: String, method: Method)(
-    implicit decoder: EntityDecoder[IO, T], 
-    encoder: EntityEncoder[IO, T]
-  ): IO[Response[IO]] = {
-    EmberClientBuilder.default[IO].build.use { client =>
-      Uri.fromString(uriString) match {
-        case Right(uri) =>
-          // modify request for file service
-          val modifiedReq = {
-            val baseReq = req.withUri(uri).withMethod(method)
-            if (uri.host.exists(_.value.contains('_'))) {
-              val filteredHeaders = baseReq.headers.headers.filterNot(_.name == CIString("Host"))
-              baseReq.withHeaders(filteredHeaders)
-            } else {
-              baseReq
-            }
-          }
-  
-          client.expect[T](modifiedReq)
-            .flatMap(Ok(_))
-            .handleErrorWith {
-              case e =>
-                println(s"Error during request to $uriString: $e")
-                InternalServerError("Internal Server Error")
-            }
-        
-        case Left(parseFailure) =>
-          println(s"Invalid URI '$uriString': ${parseFailure.details}")
-          InternalServerError("Invalid URI")
-      }
-    }
-  }
-  
-  def routeRequestJson(req: Request[IO], URI: String, method : Method): cats.effect.IO[org.http4s.Response[cats.effect.IO]] = {
-    routeRequestImpl[Json](req, URI, method)
-  }
-
-  def routeRequestString(req: Request[IO], URI: String, method : Method): cats.effect.IO[org.http4s.Response[cats.effect.IO]] = {
-    routeRequestImpl[String](req, URI, method)
-  }
-
   object FileIdQueryParamMatcher extends QueryParamDecoderMatcher[String]("file_id")
   object FolderIdQueryParamMatcher extends QueryParamDecoderMatcher[String]("folder_id")
+  object UserIddQueryParamMatcher extends QueryParamDecoderMatcher[String]("user_id")
 
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
 
@@ -81,9 +43,12 @@ object FileRoutes {
 
     case req @ GET -> Root :? FolderIdQueryParamMatcher(folderId) =>
       routeRequestJson(req, s"http://file:$file_service_port/file/?folder_id=$folderId", Method.GET)
-    
 
-    case req @ DELETE -> Root / "delete" :?  FileIdQueryParamMatcher(fileId) =>
-      routeRequestJson(req, s"http://file:$file_service_port/file/delete?file_id=$fileId", Method.DELETE)
+    case req @ DELETE -> Root / "delete" =>
+      routeRequestJson(req, s"http://file:$file_service_port/file/delete", Method.DELETE)
+
+    case req @ GET -> Root / "folder" :? UserIddQueryParamMatcher(userId) =>
+      routeRequestJson(req, s"http://file:$file_service_port/folder?user_id=$userId", Method.GET)
+    
   } 
 }
