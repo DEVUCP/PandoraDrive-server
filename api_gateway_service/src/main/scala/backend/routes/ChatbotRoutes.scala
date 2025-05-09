0853108b21ec76c11f904236eb56bbdf3528c24f
moha09-coder <mohamedelmossalamy@gmail.com>
2025-05-10 00:20:17 +0300

    feat(gateway): apply authorization middleware to ChatbotRoutes and FileRoutes

--- api_gateway_service/src/main/scala/backend/routes/ChatbotRoutes.scala
+++ api_gateway_service/src/main/scala/backend/routes/ChatbotRoutes.scala
@@ -2,17 +2,20 @@ package backend.routes
 
 import cats.effect.IO
 import org.http4s.HttpRoutes
+import org.http4s.AuthedRoutes
+import model.User.AuthUser
+import backend.middlewares.AuthMiddleware
 import org.http4s.dsl.io._
 
 object ChatbotRoutes {
-  val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root =>
+  val routesWithAuth: AuthedRoutes[AuthUser, IO] = AuthedRoutes.of {
+    case GET -> Root as _ =>
       Ok("List of chatbot")
-    case POST -> Root / "chat" =>
+    case POST -> Root / "chat" as _ =>
       Ok(s"Chat with chatbot")
-    case DELETE -> Root / "chat" / chatbotId =>
+    case DELETE -> Root / "chat" / chatbotId as _ =>
       Ok(s"Chatbot $chatbotId deleted")
-  
+  }
 
-  } 
+  val routes = AuthMiddleware(routesWithAuth)
 }
