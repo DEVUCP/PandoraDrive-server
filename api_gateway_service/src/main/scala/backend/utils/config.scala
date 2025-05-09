25bbe0a1b0d6ce5e13ef0f0e11b1a673227c2afd
moha09-coder <mohamedelmossalamy@gmail.com>
2025-05-09 19:51:27 +0300

    fix(gateway): add fallback DB_URL in config.scala

--- api_gateway_service/src/main/scala/backend/utils/config.scala
+++ api_gateway_service/src/main/scala/backend/utils/config.scala
@@ -5,7 +5,7 @@ import com.comcast.ip4s.Port
 import  com.comcast.ip4s.port
 
 object config {
-    val DB_URL = sys.env.getOrElse("DB_URL", "")
+    val DB_URL = sys.env.getOrElse("DB_URL", "jdbc:sqlite:db.db")
     val SERVICE_PORT : Port = Port.fromString(sys.env.getOrElse("GATEWAY_PORT", "55551")).getOrElse(port"55551")
     val FILE_SERVICE_PORT : Port = Port.fromString(sys.env.getOrElse("FILE_SERVICE_PORT", "55555")).getOrElse(port"55555")
 }
