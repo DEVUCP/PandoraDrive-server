6063df8a2cb56b1600ff5b43c6b5adb487a5d4b0
omardoescode <omardoescode@gmail.com>
2025-05-10 13:45:12 +0300

    chore: add .env.example

--- file_service/src/main/scala/schema/chunk_metadata.scala
+++ file_service/src/main/scala/schema/chunk_metadata.scala
@@ -1,17 +1,5 @@
 package schema
 
-import cats._
-import cats.data._
-import cats.implicits._
-
-import cats.effect.IO
-import cats.effect.unsafe.implicits.global
-
-import db.transactor
-import doobie._
-import doobie.implicits._
-import doobie.util.transactor.Transactor
-import io.circe.Encoder
 import types.ChunkId
 
 case class ChunkMetadata(
