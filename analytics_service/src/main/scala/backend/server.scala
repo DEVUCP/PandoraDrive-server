e298be400648490c27e7f5fc0825fb0a1d3e75ef
Marwan Mostafa <marwanm.dev@gmail.com>
2025-05-10 18:32:48 +0300

    refactor(analytics): Final cleanup and code formatting

--- analytics_service/src/main/scala/backend/server.scala
+++ analytics_service/src/main/scala/backend/server.scala
@@ -9,31 +9,14 @@ import com.comcast.ip4s.*
 import io.circe.generic.auto.*
 import org.http4s.circe.*
 import io.circe.Json
-import io.circe.syntax.* // Added for Json handling
+import io.circe.syntax.*
 import java.time.LocalDate
-
-def parseDate(str: String): Option[LocalDate] =
-  try Some(LocalDate.parse(str.take(10)))
-  catch { case _: Throwable => None }
-
-case class FileMetadata(
-    file_id: Int,
-    folder_id: Int,
-    file_name: String,
-    size_bytes: Int,
-    mime_type: String,
-    user_id: Int,
-    status: String,
-    uploaded_at: String,
-    created_at: String,
-    modified_at: String
-)
 
 implicit val fileDecoder: EntityDecoder[IO, List[FileMetadata]] = jsonOf
 implicit val analyticsEncoder: EntityEncoder[IO, Json] = jsonEncoderOf
 
-def routeRequestImpl[T](uriString: String, method: Method)(implicit
-    decoder: EntityDecoder[IO, T]
+def routeRequestImpl[T](uriString: String, method: Method)(
+    implicit decoder: EntityDecoder[IO, T]
 ): IO[T] = {
   EmberClientBuilder.default[IO].build.use { client =>
     Uri.fromString(uriString) match {
@@ -42,17 +25,27 @@ def routeRequestImpl[T](uriString: String, method: Method)(implicit
         client.expect[T](req)
 
       case Left(parseFailure) =>
-        IO.raiseError(
-          new RuntimeException(s"Invalid URI: ${parseFailure.details}")
-        )
+        IO.raiseError(new RuntimeException(s"Invalid URI: ${parseFailure.details}"))
     }
   }
 }
 
-// Constants
+case class FileMetadata(
+  file_id: Int,
+  folder_id: Int,
+  file_name: String,
+  size_bytes: Int,
+  mime_type: String,
+  user_id: Int,
+  status: String,
+  uploaded_at: String,
+  created_at: String,
+  modified_at: String
+)
+
+// Placeholder values
 val maxDriveSpace: Int = 536870912 // 512 MB
 
-// Helpers
 private def getCreatedDates(files: List[FileMetadata]): List[String] =
   files.map(_.created_at).filter(_.nonEmpty).sorted
 
@@ -61,6 +54,10 @@ private def isVideo(mime: String): Boolean = mime.startsWith("video/")
 
 def getAnalytics(folderId: String): IO[Response[IO]] = {
   val fileServiceUrl = s"http://localhost:55555/file?folder_id=$folderId"
+
+  def parseDate(str: String): Option[LocalDate] =
+    try Some(LocalDate.parse(str.take(10)))
+    catch { case _: Throwable => None }
 
   routeRequestImpl[List[FileMetadata]](fileServiceUrl, Method.GET)
     .flatMap { files =>
@@ -71,6 +68,29 @@ def getAnalytics(folderId: String): IO[Response[IO]] = {
       val totalSize = files.map(_.size_bytes).sum
       val spaceLeft = maxDriveSpace - totalSize
 
+      val createdDates = files.flatMap(f => parseDate(f.created_at))
+      val mostRecentCreated =
+        createdDates.sorted.lastOption.map(_.toString).getOrElse("N/A")
+
+      val uploadedDates = files.flatMap(f => parseDate(f.uploaded_at))
+      val mostRecentUploaded =
+        uploadedDates.sorted.lastOption.map(_.toString).getOrElse("N/A")
+
+      val photos = files.count(f => isPhoto(f.mime_type))
+      val videos = files.count(f => isVideo(f.mime_type))
+
+      val currentDate = LocalDate.now()
+      def uploadedThis(pred: LocalDate => Boolean): Int =
+        uploadedDates.count(pred)
+
+      val uploadedToday = uploadedThis(_.isEqual(currentDate))
+      val uploadedWeek = uploadedThis(_.isAfter(currentDate.minusDays(7)))
+      val uploadedMonth = uploadedThis(_.isAfter(currentDate.minusDays(30)))
+      val uploadedYear = uploadedThis(_.isAfter(currentDate.minusDays(365)))
+
+      val largestVideo = sortedBySize.reverse.find(f => isVideo(f.mime_type))
+      val smallestVideo = sortedBySize.find(f => isVideo(f.mime_type))
+
       val responseJson = Json.obj(
         "The largest file in your drive" -> Json.fromString(
           largest.map(_.file_name).getOrElse("N/A")
@@ -78,78 +98,49 @@ def getAnalytics(folderId: String): IO[Response[IO]] = {
         "The smallest file in your drive" -> Json.fromString(
           smallest.map(_.file_name).getOrElse("N/A")
         ),
-        "The total size of your uploaded media is" -> Json.fromInt(totalSize),
-        "The space you have left in your drive" -> Json.fromInt(spaceLeft),
         "The number of files you uploaded to your drive" -> Json.fromInt(
           numFiles
         ),
         "Your most recently uploaded photo/video was taken at" -> Json
-          .fromString(
-            files.flatMap(f => f.taken_at).sorted.lastOption.getOrElse("N/A")
-          ),
+          .fromString(mostRecentCreated),
         "Your last photo/video uploaded was uploaded at" -> Json.fromString(
-          files.flatMap(f => f.created_at).sorted.lastOption.getOrElse("N/A")
+          mostRecentUploaded
         ),
+        "The total size of your uploaded media is" -> Json.fromInt(totalSize),
         "The number of photos/videos you uploaded this day" -> Json.fromInt(
-          files.count(f =>
-            f.created_at.exists(_.startsWith(currentDate("day"))) && (isPhoto(
-              f.mime_type
-            ) || isVideo(f.mime_type))
-          )
+          uploadedToday
         ),
         "The number of photos/videos you uploaded this week" -> Json.fromInt(
-          files.count(f =>
-            f.created_at.exists(_.startsWith(currentDate("week"))) && (isPhoto(
-              f.mime_type
-            ) || isVideo(f.mime_type))
-          )
+          uploadedWeek
         ),
         "The number of photos/videos you uploaded this month" -> Json.fromInt(
-          files.count(f =>
-            f.created_at.exists(_.startsWith(currentDate("month"))) && (isPhoto(
-              f.mime_type
-            ) || isVideo(f.mime_type))
-          )
+          uploadedMonth
         ),
         "The number of photos/videos you uploaded this year" -> Json.fromInt(
-          files.count(f =>
-            f.created_at.exists(_.startsWith(currentDate("year"))) && (isPhoto(
-              f.mime_type
-            ) || isVideo(f.mime_type))
-          )
+          uploadedYear
         ),
-        "The length of your longest uploaded video" -> Json.fromInt(
-          files
-            .filter(f => isVideo(f.mime_type))
-            .flatMap(_.duration_seconds)
-            .maxOption
-            .getOrElse(0)
+        "The length of your longest uploaded video" -> Json.fromString(
+          largestVideo.map(v => v.size_bytes + " bytes").getOrElse("N/A")
         ),
-        "The length of your shortest uploaded video" -> Json.fromInt(
-          files
-            .filter(f => isVideo(f.mime_type))
-            .flatMap(_.duration_seconds)
-            .minOption
-            .getOrElse(0)
+        "The length of your shortest uploaded video" -> Json.fromString(
+          smallestVideo.map(v => v.size_bytes + " bytes").getOrElse("N/A")
         ),
-        "The number of videos you have in your drive" -> Json.fromInt(
-          files.count(f => isVideo(f.mime_type))
-        ),
-        "The number of photos you have in your drive" -> Json.fromInt(
-          files.count(f => isPhoto(f.mime_type))
-        ),
+        "The number of videos you have in your drive" -> Json.fromInt(videos),
+        "The number of photos you have in your drive" -> Json.fromInt(photos),
         "The number of folders you have created in your drive" -> Json.fromInt(
-          files.map(_.folder_id).distinct.length
+          files.map(_.folder_id).distinct.size
         ),
+        "The space you have left in your drive" -> Json.fromInt(spaceLeft),
         "The folder that has the biggest number of uploaded media" -> Json
-          .fromString(
+          .fromInt(
             files
               .groupBy(_.folder_id)
-              .maxByOption(_._2.length)
+              .maxByOption(_._2.size)
               .map(_._1)
-              .getOrElse("N/A")
+              .getOrElse(-1)
           )
       )
+
       Ok(responseJson)
     }
     .handleErrorWith { e =>
@@ -161,25 +152,22 @@ def getAnalytics(folderId: String): IO[Response[IO]] = {
 }
 
 object server extends IOApp:
-  object FolderIdQueryParameter
-      extends QueryParamDecoderMatcher[String]("folder_id")
+  object FolderIdQueryParameter extends QueryParamDecoderMatcher[String]("folder_id")
 
-  private val helloWorldService = HttpRoutes
-    .of[IO] {
-      case GET -> Root / "hello" / name =>
-        Ok(s"Hello, $name!")
-      case GET -> Root / "hello" =>
-        Ok("Hello, World!")
-      case GET -> Root / "ping" =>
-        Ok("pong")
-      case GET -> Root / "analytics" :? FolderIdQueryParameter(id) =>
-        getAnalytics(id)
-    }
-    .orNotFound
+  private val helloWorldService = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name!")
+    case GET -> Root / "hello" =>
+      Ok("Hello, World!")
+    case GET -> Root / "ping" =>
+      Ok("pong")
+    case GET -> Root / "analytics" :? FolderIdQueryParameter(id) =>
+      getAnalytics(id)
+  }.orNotFound
 
   var port = sys.env.get("ANALYTICS_SERVICE_PORT") match {
     case Some(port) => Port.fromString(port).getOrElse(port"55552")
-    case None       => port"55552"
+    case None => port"55552"
   }
 
   def run(args: List[String]): IO[ExitCode] =
