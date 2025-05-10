749548ffe2e995e15b6b485b784cd167d46304f3
omardoescode <omardoescode@gmail.com>
2025-05-10 11:49:00 +0300

    feat: Add folder deletion to script `fs.py`

--- file_service/fs.py
+++ file_service/fs.py
@@ -5,6 +5,7 @@ import requests
 
 cur_folder: Dict = {}
 cwd_files = []
+cwd_folders = []
 cwd_valid = False
 
 USER_ID = 1
@@ -29,6 +30,10 @@ def validate_cwd():
     req = requests.get(f"{URL}/file?folder_id={cur_folder['folder_id']}")
     for file in req.json():
         cwd_files.append(file)
+
+    req = requests.get(f"{URL}/folder?parent_folder_id={cur_folder['folder_id']}")
+    for folder in req.json():
+        cwd_folders.append(folder)
     cwd_valid = True
 
 
@@ -118,25 +123,84 @@ def pwd():
     print(cur_folder["folder_name"])
 
 
+def mkdir(cmd: str):
+    parts = cmd.split(" ")
+    if len(parts) != 2:
+        print("Invalid format: mkdir <folder_name>")
+        return
+
+    folder_name = parts[1]
+
+    validate_folder()
+    body = {
+        "folder_name": folder_name,
+        "user_id": USER_ID,
+        "parent_folder_id": cur_folder["folder_id"],
+    }
+    req = requests.post(f"{URL}/folder/upload", json=body)
+    data = req.json()
+    if req.status_code == 200:
+        print("Folder created successfully")
+        print(data)
+    else:
+        print(data["error"])
+
+
+def rmdir(cmd: str):
+    parts = cmd.split(" ")
+    if len(parts) != 2:
+        print("Invalid format: rmdir <folder_name>")
+        return
+
+    validate_folder()
+    validate_cwd()
+
+    folder_name = parts[1]
+    folder_id = next(
+        (
+            folder["folder_id"]
+            for folder in cwd_folders
+            if folder["folder_name"] == folder_name
+        ),
+        None,
+    )
+
+    if not folder_id:
+        print("Folder not found in current directory")
+        return
+
+    req = requests.delete(
+        f"{URL}/folder/delete",
+        json={"folder_id": folder_id, "user_id": USER_ID},
+    )
+    if req.status_code == 200:
+        print("Folder has been removed")
+        global cwd_valid
+        cwd_valid = False
+    else:
+        print(req.content)
+
+
 if __name__ == "__main__":
     running = True
     while running:
         cmd = input("> ").strip().lower()
 
-        try:
-            if cmd in ["quit", "exit", "q"]:
-                running = False
-            elif cmd == "ls":
-                ls()
-            elif cmd == "restart":
-                restart()
-            elif cmd.startswith("rm"):
-                rm(cmd)
-            elif cmd.startswith("dl"):
-                download(cmd)
-            elif cmd.startswith("pwd"):
-                pwd()
-            else:
-                print(f"Invaild cmd: {cmd}")
-        except:
-            print("Something wrong happened")
+        if cmd in ["quit", "exit", "q"]:
+            running = False
+        elif cmd == "ls":
+            ls()
+        elif cmd == "restart":
+            restart()
+        elif cmd.startswith("rmdir"):
+            rmdir(cmd)
+        elif cmd.startswith("rm"):
+            rm(cmd)
+        elif cmd.startswith("dl"):
+            download(cmd)
+        elif cmd.startswith("pwd"):
+            pwd()
+        elif cmd.startswith("mkdir"):
+            mkdir(cmd)
+        else:
+            print(f"Invaild cmd: {cmd}")
