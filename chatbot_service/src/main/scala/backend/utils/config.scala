573f334c2b9134cafa210f3ecf7be679d38402c4
moha09-coder <mohamedelmossalamy@gmail.com>
2025-05-14 12:36:54 +0300

    feat(chatbot): add analytics rules to rule engine

--- chatbot_service/src/main/scala/backend/utils/config.scala
+++ chatbot_service/src/main/scala/backend/utils/config.scala
@@ -6,5 +6,8 @@ import  com.comcast.ip4s.port
 object config {
   val TOKENS_FILE_PATH : String = sys.env.getOrElse("TOKENS_FILE_PATH", "src/main/scala/backend/data/tokens.json")
   val SERVICE_PORT : Port = Port.fromString(sys.env.getOrElse("CHATBOT_SERVICE_PORT", "55550")).getOrElse(port"55550")
-  val FILE_SERVICE_PORT : Port = Port.fromString(sys.env.getOrElse("FILE_SERVICE_PORT", "55555")).getOrElse(port"55555")
+  val ANALYTICS_SERVICE_PORT : Port = Port.fromString(sys.env.getOrElse("ANALYTICS_SERVICE_PORT", "55552")).getOrElse(port"55552")
+  val ANALYTICS_SERVICE_HOST: String = sys.env.getOrElse("ANALYTICS_SERVICE_HOST", "http://localhost")
+  val ANALYTICS_SERVICE_PATH: String = sys.env.getOrElse("ANALYTICS_SERVICE_PATH", "analytics")
+  val ANALYTICS_SERVICE_QUERY_PARAM: String = sys.env.getOrElse("ANALYTICS_SERVICE_QUERY_PARAM", "user_id")
 }
